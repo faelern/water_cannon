2672ad3ce907bc126027ba3ec3ae05206dbe4bf6
faelern <mico.mg52@gmail.com>
2023-12-30 14:20:45 +0100

    minor changes

--- stm_aim.py
+++ stm_aim.py
@@ -6,7 +6,7 @@ from vardata import *
 
 # pos - object coordinate on camera
 # distance - relative distance from camera to object
-# abs_distance - absolute distance from camera plane to object
+# abs_distance - absolute distance from camera plane to object (absolute in Y axis)
 # elevation - object elevation
 
 
@@ -22,13 +22,11 @@ def calculate_elevation(y_pos, distance):
 
 def aim(x_pos, y_pos, distance):
     elevation = calculate_elevation(y_pos, distance)
-    print(elevation)
     abs_distance = calculate_abs_distance(y_pos, distance)
-    print(abs_distance)
     h_angle = calculate_h_angle(x_pos)
     v_angle = calculate_v_angle(abs_distance, elevation)
+    if UNIT == 'deg':
+        return h_angle, v_angle
+    else:
+        return h_angle / pi * 180, v_angle / pi * 180
 
-    return h_angle, v_angle
-
-
-print(aim(-300, 200, 4))
